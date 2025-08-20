b8016c9875e6668893e9e495d3b9160e4012bbf3
Ahmed Saber <devahmedsaber@gmail.com>
2025-08-20 19:53:42 +0300

    Enhancements and installing laravel horizon

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('sync:products')->everyFiveMinutes();
+Schedule::command('sync:products')->everyMinute();
